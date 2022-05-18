06dccf66bb347f2d6a9377d043311145ea1c1b41
Matt <mgstankov30@gmail.com>
2022-05-18 15:12:34 -0400

    add dyanmic function to populate the string for number of days remaining to pass the jest tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,27 @@ export default function DayListItem(props) {
     "day-list__item--full" : props.spots === 0,
   });
 
+
+  const formatSpots = function(num) {
+    let result = '';
+    if(num === 0) {
+      result += "no spots remaining"
+      return result;
+    }
+    if(num === 1) {
+      result += "1 spot remaining"
+      return result;
+    }
+    if(num > 1) {
+      result = num + ` spots remaining` 
+      return result;
+    }
+  }
+
   return(
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className={"text--regular"}>{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
