ccdb7b9642b8fa6fd52d768ca084b058c0399825
Matt <mgstankov30@gmail.com>
2022-05-19 13:35:09 -0400

    refactor code to pass less props down and ensure same functionality

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,13 +3,13 @@ import classNames from "classnames";
 
 import "components/InterviewerListItem.scss";
 
-export default function InterviewerListItme(props) {
+export default function InterviewerListItem(props) {
   const interviewClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   });
 
   return (
-    <li className={interviewClass} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
