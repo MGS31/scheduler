4e0e2f78f78411ca7cce56a9539d775b662602c1
Matt <mgstankov30@gmail.com>
2022-05-24 12:30:13 -0400

    add useEffect method to call axios get request to pull in days data from the database

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import  React, {useState} from "react";
+import React, {useState, useEffect } from "react";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
+import axios from 'axios'
 
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -74,6 +57,14 @@ const appointList = appointArray.map((appointment) => {
 export default function Application(props) {
 
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days').then((response) => {
+      // console.log("response: ", response.data);
+      setDays([...response.data]);
+    })
+  }, []);
 
   return (
     <main className="layout">
