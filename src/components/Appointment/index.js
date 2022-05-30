276f57eba73f57de7d75cf46939cd47b5190f66c
Matt <mgstankov30@gmail.com>
2022-05-30 17:10:30 -0400

    add save, delete, confirm and edit functionality and functions for the user to manipulate different stages of an interview appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Confirm from "./Confirm";
+import Status from "./Status";
 import useVisualMode from "components/Hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
@@ -10,20 +12,54 @@ import "components/Appointment/styles.scss";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETE = "DELETE";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
-  return (
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview).then(()=> {
+      transition(SHOW);
+    }) 
+
+  };
+
+  function edit(){
+    transition(EDIT);
+  };
+
+  function confirm(){
+    transition(CONFIRM);
+  };
+
+  function deleteInterview(){
+    transition(DELETE);
+    props.cancelInterview(props.id).then(()=> {
+      transition(EMPTY);
+    })
+  };
+
+
+  return (  
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          student={props.interview && props.interview.student}
+          interviewer={props.interview && props.interview.interviewer}
+          onDelete={confirm}
+          onEdit={edit}
         />
       )}
       {mode === CREATE && 
@@ -31,9 +67,24 @@ export default function Appointment(props) {
         student={props.student}
         interviewer={props.interviewer}
         interviewers={props.interviewers}
-        onSave={() => {}}
+        onSave={save}
         onCancel={() => back()}
         />}
+      {mode === SAVING && <Status message="Saving" />}
+      {mode === DELETE && <Status message="Deleteing" />}
+      {mode === CONFIRM && 
+      <Confirm 
+        message="Are You Sure You Wan't To Delete?"
+        onCancel={() => back()}
+        onConfirm={deleteInterview}
+      />}
+      {mode === EDIT && 
+      <Form 
+      student={props.interview.student}
+      interviewers={props.interviewers}
+      onSave={save}
+      onCancel={() => back()}
+      />}
     </article>
   );
 }
